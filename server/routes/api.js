96bd8fe39d44faa26dd26dda6ac1fc74358c7a36
Sinan Bolel <dev@sinanbolel.com>
2015-07-19 01:39:48 -0400

    Added CORS to api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var debug = require('debug')('api');
+var cors = require('cors');
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/ping', function(req, res){
-  debug('/ping request received.');
+  debug('GET /ping');
   res.json({status: 200});
 });
 
