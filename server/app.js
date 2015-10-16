ec4a68aaddb9c84ca93e96c9360b3b9719be0f1f
Sinan Bolel <sinanbolel@gmail.com>
2015-10-15 21:37:26 -0500

    Removed unnecessary files. Updated dependencies.

--- server/app.js
+++ server/app.js
@@ -1,18 +1,15 @@
+#!/usr/bin/env node
 'use strict';
 
 var debug = require('debug')('app'),
     express = require('express'),
     _routes = require('./routes'),
-    _firebaseApi = require('./modules/firebase-api'),
     _packageInfo = require('./../package.json');
 
-// Express
+// setup express
 var app = express();
 app.use('/', _routes);
 
-app._packageInfo = _packageInfo;
-
-// Module exports
+// app exports
+app.packageInfo = _packageInfo;
 module.exports = app;
-
-_firebaseApi.init();
